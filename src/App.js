2cc0173964d746d2531badc8ccc1a7f0779f6b2c
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-05-24 11:34:31 +0530

    Structure for the application finalised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import './App.css';
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <h1 className="underline text-3xl">App</h1>
     );
 };
 
